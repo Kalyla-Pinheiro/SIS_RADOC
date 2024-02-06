b1237b451a61ca8777403273a5aa2f912464069a
Joao Henrique <jh07102003@gmail.com>
2024-02-06 15:44:24 -0300

    Reparo das telas de login e cadastro

--- sisradoc2/src/pages/login/Login.jsx
+++ sisradoc2/src/pages/login/Login.jsx
@@ -1,37 +1,42 @@
-import React from 'react';
-import './Login.css';
+import React from "react";
+import "./Login.css";
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 
 const Login = () => {
-    return (
-        <div className="login-container">
-            <div className='wrapper'>
-                <form action="">
-                    <h1>Sisradoc</h1>
-                    <div className="input-box">
-                        <input type="text" placeholder='Usuário' required/>
-                        <FaUser className='icon'/>
-                    </div>
-                    <div className='input-box'>
-                        <input type="password" placeholder='Senha' required/>
-                        <FaLock className='icon'/>
-                    </div>
+  return (
+    <div className="login-container">
+      <div className="wrapper-login">
+        <form action="">
+          <h1>Sisradoc</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Usuário" required />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Senha" required />
+            <FaLock className="icon" />
+          </div>
 
-                    <div className="remember-forgot">
-                        <label><input type="checkbox" />Lembre de mim</label>
-                        <a href="#">Esqueceu a senha?</a>
-                    </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" />
+              Lembre de mim
+            </label>
+            <a href="#">Esqueceu a senha?</a>
+          </div>
 
-                    <button type='submit'>Login</button>
+          <button type="submit">Login</button>
 
-                    <div className="register-link">
-                        <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
-}
+          <div className="register-link">
+            <p>
+              Não tem uma conta? <a href="#">Cadastre-se</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
