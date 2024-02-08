2d5e28e1fceb58a30538dd76a91ee1d56eff93a4
Joao Henrique <jh07102003@gmail.com>
2024-02-07 22:40:18 -0300

    criacao de prototipo de tela home

--- sisradoc2/src/pages/home/Home.jsx
+++ sisradoc2/src/pages/home/Home.jsx
@@ -3,11 +3,21 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="home-container">
+    <main className="home-container">
       <div className="wrapper-home">
-        <h1>Tela Home</h1>
+        <h1>RADOC</h1>
+        <p>
+          O Relatório de Atividades Docente (RADOC) é um documento
+          institucional, utilizado não somente para compor os processos de
+          progressão/promoção funcional, mas para registrar todas as atividades
+          (ensino, pesquisa, extensão e administração) que são realizadas pelos
+          docentes da UFRA, além de subsidiar a administração superior na
+          aplicação do modelo de alocação de vagas e recursos.
+        </p>
+        <button>Saiba Mais</button>
       </div>
-    </div>
+      <div></div>
+    </main>
   );
 };
 
