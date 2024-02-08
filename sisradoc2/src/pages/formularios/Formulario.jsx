c5618745a71f606035a5d7984680c9e74528ed7f
Polar0471 <rodrigoarth512@gmail.com>
2024-02-07 22:17:30 -0300

    Criando protótipo de tela de formulários

--- sisradoc2/src/pages/formularios/Formulario.jsx
+++ sisradoc2/src/pages/formularios/Formulario.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import './Formulario.css'
+import React from "react";
+import "./Formulario.css";
 
 const Formulario = () => {
-    return (
-        <div className="formulario-container">
-            <div className='wrapper'>
-                <h1>Tela de Formulários</h1>
+  return (
+    <div className="formulario-container">
+      <div className="wrapper-formulario">
+        <div className="campos">
+            <div className="item-campos" id="primeiro-campo">
+                <h1>Ensino</h1>
+            </div>
+            <div className="item-campos">
+                <h1>Pesquisa</h1>
+            </div>
+            <div className="item-campos">
+                <h1>Extensão</h1>
+            </div>
+            <div className="item-campos">
+                <h1>Gestão</h1>
+            </div>
+            <div className="item-campos" id="ultimo-campo">
+                <h1>Outros</h1>
             </div>
         </div>
-    );
-}
+
+        <div className="campo-preenchimento">
+            <h1>Campo de preenchimento</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Formulario;
