d6ba67dabdb164f9f39d1e724f228a71153b85aa
Joao Henrique <jh07102003@gmail.com>
2024-02-18 22:01:17 -0300

    Tela de DocentesEnvolvidos

--- sisradoc2/src/pages/formularios/DocentesEnvolvidos/DocentesEnvolvidos.jsx
+++ sisradoc2/src/pages/formularios/DocentesEnvolvidos/DocentesEnvolvidos.jsx
@@ -1,13 +1,54 @@
 import React from "react";
-import classes from "../../../css-modules/DocentesEnvolvidos.module.css";
+import classes from "../../../css-modules/Ensino.module.css";
+import Navegacao from "../../../components/Navegação/Navegacao";
+import { BsQuestionCircleFill } from "react-icons/bs";
 
 const DocentesEnvolvidos = () => {
   return (
-    <div className={classes.DocenteContainer}>
-      <h1>Docentes Envolvidos</h1>
-      <div className={classes.wrapperDocente}>
-        <h1>Semestre 1</h1>
-        <h1>Semestre 2</h1>
+    <div>
+      <Navegacao />
+      <div className={classes.DocenteContainer}>
+        <div className={classes.tituloDocente}>
+          <h1>Docentes Envolvidos</h1>
+        </div>
+
+        <div className={classes.semestre} id={classes.primeiroSemestre}>
+          <div className={classes.tituloSemestre}>
+            <h2>1º Semestre</h2>
+          </div>
+          <div className={classes.camposTabela}>
+            <input type="text" placeholder="Nome" required />{" "}
+            <input type="text" placeholder="CH" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+          </div>
+          <div className={classes.tituloSemestre}>
+            <h4>Dividiu a disciplina com outro Docente?</h4>
+          </div>
+        </div>
+
+        <div className={classes.semestre} id={classes.primeiroSemestre}>
+          <div className={classes.tituloSemestre}>
+            <h2>2º Semestre</h2>
+          </div>
+          <div className={classes.camposTabela}>
+            <input type="text" placeholder="Nome" required />{" "}
+            <input type="text" placeholder="CH" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+          </div>
+          <div className={classes.tituloSemestre}>
+            <h4>Dividiu a disciplina com outro Docente?</h4>
+          </div>
+        </div>
+
+        <div className={classes.semestre} id={classes.primeiroSemestre}>
+          <div className={classes.camposTabela}>
+            <input type="text" placeholder="Nome do outro docente" required />{" "}
+            <input type="text" placeholder="Nome do outro docente" required />{" "}
+          </div>
+        </div>
+        <div className={classes.buttons}>
+          <button>Próximo</button>
+        </div>
       </div>
     </div>
   );
