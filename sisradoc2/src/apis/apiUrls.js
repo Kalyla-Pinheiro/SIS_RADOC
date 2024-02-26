5ac1e9d477b563f0eb53d00898a70f0e8ef7ca37
JeanKasoki <jeanmyjean@hotmail.com>
2024-02-25 22:23:16 -0300

    feat: adicao do tostify

--- sisradoc2/src/apis/apiUrls.js
+++ sisradoc2/src/apis/apiUrls.js
@@ -1,9 +1,10 @@
-const baseUrl = 'http://localhost:5000';
+const baseUrl = "http://localhost:5000";
 
 const apiUrls = {
-    login: `${baseUrl}/login`,
-    cadastro: `${baseUrl}/cadastro`,
-    ensino: `${baseUrl}/ensino`
-}
+  login: `${baseUrl}/login`,
+  cadastro: `${baseUrl}/cadastro`,
+  ensino: `${baseUrl}/ensino`,
+  disc_ministradas: `${baseUrl}/pdf/ministradas`,
+};
 
 export default apiUrls;
--- sisradoc2/src/pages/disciplinas-ministradas/DisciplinasMinistradas.jsx
+++ sisradoc2/src/pages/disciplinas-ministradas/DisciplinasMinistradas.jsx
@@ -1,54 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../../css-modules/Ensino.module.css";
 import Navegacao from "../../components/Navegação/Navegacao";
-import PopUp from "../../components/popUp/popUp"
+import PopUp from "../../components/popUp/popUp";
 import { BsQuestionCircleFill } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import apiUrls from "../../apis/apiUrls";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const DisciplinasMinistradas = () => {
 
-  const navigate = useNavigate();
+  const [selectedFile, setSelectedFile] = useState(null);
+  const notifySuccess = () => toast.success("Arquivo enviado com sucesso!");
+  const notifyError = () => toast.error("Arquivo não foi enviado.");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("file", selectedFile);
+
+    try {
+      const response = await fetch(apiUrls.disc_ministradas, {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: formData,
+      });
+
+      if (response.ok) {
+        notifySuccess();
+      } 
+    } catch (error) {
+      notifyError();
+    }
+  };
 
   return (
     <div>
-      <Navegacao/>
-      <PopUp/>
+      <Navegacao />
+      <PopUp />
 
       <div className={classes.disciplinasMinistradasConteiner}>
         <div className={classes.titulo}>
           <h1>Disciplinas Ministradas</h1>
         </div>
 
-        <form className={classes.anexarPdfs} action="" method="post" enctype="multipart/form-data">
+        <form
+          className={classes.anexarPdfs}
+          action=""
+          method="post"
+          enctype="multipart/form-data"
+          onSubmit={handleSubmit}
+        >
           <div className={classes.inputsPdfs} id={classes.primeiroInput}>
-            <input type="file" accept=".pdf"/>
+            <input type="file" accept=".pdf" />
             <p>Declaração de disciplinas ministradas (PDF)</p>
           </div>
           <div className={classes.inputsPdfs} id={classes.segundoInput}>
-            <input type="file" accept=".pdf"/>
+            <input type="file" accept=".pdf" />
             <p>Diarios de turma (PDF)</p>
           </div>
 
-          <button className={classes.buttons}>
-            Submeter
+          <button type="submit" className={classes.buttons}>
+            Enviar
           </button>
         </form>
-        
+
         <div className={classes.semestre} id={classes.primeiroSemestre}>
           <div className={classes.tituloSemestre}>
             <p>1º Semestre</p>
           </div>
           <div className={classes.camposTabela}>
-            <input type="text" placeholder="Código ou Nome" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" placeholder="Sigla do curso" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" placeholder="Nível" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" className={classes.ultimoCampoInput} placeholder="CH total" required /> <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Nome" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Sigla do curso" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Nível" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input
+              type="text"
+              className={classes.ultimoCampoInput}
+              placeholder="CH total"
+              required
+            />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
           </div>
           <div className={classes.camposTabela}>
-            <input type="text" placeholder="Código ou Nome" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" placeholder="Sigla do curso" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" placeholder="Nível" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" className={classes.ultimoCampoInput} placeholder="CH total" required /> <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Nome" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Sigla do curso" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Nível" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input
+              type="text"
+              className={classes.ultimoCampoInput}
+              placeholder="CH total"
+              required
+            />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
           </div>
         </div>
 
@@ -57,16 +110,34 @@ const DisciplinasMinistradas = () => {
             <p>2º Semestre</p>
           </div>
           <div className={classes.camposTabela}>
-            <input type="text" placeholder="Código ou Nome" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" placeholder="Sigla do curso" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" placeholder="Nível" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" className={classes.ultimoCampoInput} placeholder="CH total" required /> <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Nome" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Sigla do curso" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Nível" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input
+              type="text"
+              className={classes.ultimoCampoInput}
+              placeholder="CH total"
+              required
+            />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
           </div>
           <div className={classes.camposTabela}>
-            <input type="text" placeholder="Código ou Nome" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" placeholder="Sigla do curso" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" placeholder="Nível" required /> <BsQuestionCircleFill className={classes.icon} />
-            <input type="text" className={classes.ultimoCampoInput} placeholder="CH total" required /> <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Nome" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Sigla do curso" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input type="text" placeholder="Nível" required />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
+            <input
+              type="text"
+              className={classes.ultimoCampoInput}
+              placeholder="CH total"
+              required
+            />{" "}
+            <BsQuestionCircleFill className={classes.icon} />
           </div>
         </div>
 
@@ -76,6 +147,7 @@ const DisciplinasMinistradas = () => {
           </a>
         </div>
       </div>
+      <ToastContainer position="bottom-left"/>
     </div>
   );
 };
