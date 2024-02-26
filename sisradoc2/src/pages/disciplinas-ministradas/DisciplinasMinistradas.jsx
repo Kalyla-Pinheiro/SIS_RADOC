6fd49aa83f9fd53c5836d67d41a4925b65920ded
JeanKasoki <jeanmyjean@hotmail.com>
2024-02-26 00:00:59 -0300

    feat: dados do pdf disciplinas extraidos

--- sisradoc2/src/pages/disciplinas-ministradas/DisciplinasMinistradas.jsx
+++ sisradoc2/src/pages/disciplinas-ministradas/DisciplinasMinistradas.jsx
@@ -9,30 +9,59 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const DisciplinasMinistradas = () => {
-
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [pdfDisciplinas, setPdfDisciplinas] = useState(null);
+  const [pdfDiarios, setPdfDiarios] = useState(null);
   const notifySuccess = () => toast.success("Arquivo enviado com sucesso!");
   const notifyError = () => toast.error("Arquivo não foi enviado.");
 
-  const handleSubmit = async (event) => {
+  const handlepdfDisciplinasChange = (event) => {
+    setPdfDisciplinas(event.target.files[0]);
+  };
+
+  const handleDisciplinasMinistradas = async (event) => {
     event.preventDefault();
 
     const formData = new FormData();
 
-    formData.append("file", selectedFile);
+    formData.append("file", pdfDisciplinas);
 
     try {
       const response = await fetch(apiUrls.disc_ministradas, {
         method: "POST",
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
+        // headers: {
+        //   "Content-Type": "multipart/form-data",
+        // },
         body: formData,
       });
 
       if (response.ok) {
         notifySuccess();
-      } 
+        console.log(response.json());
+      }
+    } catch (error) {
+      notifyError();
+    }
+  };
+
+  const handleDiarioDeTurma = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("file", pdfDiarios);
+
+    try {
+      const response = await fetch(apiUrls.diario_turma, {
+        method: "POST",
+        // headers: {
+        //   "Content-Type": "multipart/form-data",
+        // },
+        body: formData,
+      });
+
+      if (response.ok) {
+        notifySuccess();
+      }
     } catch (error) {
       notifyError();
     }
@@ -48,21 +77,48 @@ const DisciplinasMinistradas = () => {
           <h1>Disciplinas Ministradas</h1>
         </div>
 
-        <form className={classes.campoSubmissaoPDF} action="" method="post" enctype="multipart/form-data">
-          <div className={classes.anexarPdfs}>
-            <div className={classes.inputsPdfs} id={classes.primeiroInput}>
-              <input type="file" accept=".pdf"/>
-              <p>Declaração de disciplinas ministradas (PDF)</p>
+        <div className={classes.formulariosPDF}>
+          <form
+            className={classes.campoSubmissaoPDF}
+            action=""
+            method="post"
+            encType="multipart/form-data"
+            onSubmit={handleDisciplinasMinistradas}>
+            <div className={classes.anexarPdfs}>
+              <div className={classes.inputsPdfs} id={classes.primeiroInput}>
+                <input 
+                type="file" 
+                accept=".pdf" 
+                onChange={handlepdfDisciplinasChange}
+                />
+                <p>Declaração de disciplinas ministradas (PDF)</p>
+              </div>
             </div>
-            <div className={classes.inputsPdfs} id={classes.segundoInput}>
-              <input type="file" accept=".pdf"/>
-              <p>Diarios de turma (PDF)</p>
+            <div className={classes.buttonSubmeterPDF}>
+              <button type="submit">Submeter PDF</button>
             </div>
-          </div>
-          <div className={classes.buttonSubmeterPDF}>
-            <button>Submeter PDF</button>
-          </div>
-        </form>
+          </form>
+
+          <form
+            action=""
+            method="post"
+            encType="multipart/form-data"
+            onSubmit={handleDiarioDeTurma}
+            className={classes.campoSubmissaoPDF}
+            >
+            <div className={classes.anexarPdfs}>
+              <div className={classes.inputsPdfs} id={classes.segundoInput}>
+                <input type="file" accept=".pdf" value={pdfDiarios} />
+                <p>Diarios de turma (PDF)</p>
+              </div>
+            </div>
+
+            <div className={`${classes.buttonSubmeterPDF} ${classes.secondButton}`}>
+              <button>Submeter PDF</button>
+            </div>
+
+          </form>
+        </div>
 
         <div className={classes.semestre} id={classes.primeiroSemestre}>
           <div className={classes.tituloSemestre}>
@@ -142,7 +198,7 @@ const DisciplinasMinistradas = () => {
           </a>
         </div>
       </div>
-      <ToastContainer position="bottom-left"/>
+      <ToastContainer position="bottom-left" />
     </div>
   );
 };
