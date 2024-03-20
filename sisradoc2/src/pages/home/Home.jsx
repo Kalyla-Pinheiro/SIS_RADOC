7767e986f4784f9f920c1dd69c356f80935c5731
Joao Henrique <jh07102003@gmail.com>
2024-03-20 00:23:49 -0300

    feat: tela de home atualizada

--- sisradoc2/src/pages/home/Home.jsx
+++ sisradoc2/src/pages/home/Home.jsx
@@ -7,9 +7,35 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
   return (
-    <div>
-      <div className={classes.homeContainer}>
-        <h1>Campo de preenchimento</h1>
+    <div className={classes.homeContainer}>
+      <div className={classes.titulo}>
+        <h1>Bem Vindo!</h1>
+      </div>
+      <div>
+        <h2 className={classes.paragrafos}>
+          O processo do Relatório Anual de Atividades Docente - RADOC tem seu
+          início a partir do Docente interessado, visando a comprovação e a
+          aprovação de suas atividades perante a UFRA.
+        </h2>
+        <div className={classes.conteinerRadoc}>
+          <div className={classes.botao}>
+            <div>
+              <a href="/formularios">
+                <button>Cadastrar Radoc</button>
+              </a>
+            </div>
+            <div>
+              <a href="#">
+                <button>Continuar</button>
+              </a>
+            </div>
+          </div>
+
+          <div
+            className={classes.tabelaRadoc}
+            id={classes.tabelasDisciplinasMinistradas}
+          ></div>
+        </div>
       </div>
       <ToastContainer position="bottom-left" />
     </div>
