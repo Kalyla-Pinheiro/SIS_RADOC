0d8f22bbb1e7cb497b728d26e3ce77c1c43f716e
Polar0471 <rodrigoarth512@gmail.com>
2024-03-21 01:40:58 -0300

    Atualizando home

--- sisradoc2/src/pages/home/Home.jsx
+++ sisradoc2/src/pages/home/Home.jsx
@@ -8,34 +8,69 @@ import { useEffect, useState } from "react";
 const Home = () => {
   return (
     <div className={classes.homeContainer}>
+
       <div className={classes.titulo}>
         <h1>Bem Vindo!</h1>
       </div>
-      <div>
-        <h2 className={classes.paragrafos}>
-          O processo do Relatório Anual de Atividades Docente - RADOC tem seu
-          início a partir do Docente interessado, visando a comprovação e a
-          aprovação de suas atividades perante a UFRA.
-        </h2>
-        <div className={classes.conteinerRadoc}>
-          <div className={classes.botao}>
-            <div>
-              <a href="/formularios">
-                <button>Cadastrar Radoc</button>
-              </a>
+
+      <div className={classes.campoColumn} id={classes.campoGeral}>
+        <div className={classes.campoFlex} id={classes.texto1}>
+          <h3>
+            O SISRADOC tem como principal objetivo automatizar tarefas relacionadas ao preenchimento do RADOC, 
+            oferecendo funcionalidades que simplificam o registro, edição e cálculo de atividades desenvolvidas 
+            pelo docente.
+          </h3>
+        </div>
+
+        <div className={classes.campoFlex}>
+          <div className={classes.colunaIndividual} id={classes.colunaEspacoLateral}>
+            <div className={classes.subTextos1}>
+              <h2>
+                Precisão no Cálculo das Cargas Horárias
+              </h2>
+              <h3>
+              O sistema executa automaticamente o cálculo das cargas horárias das atividades registradas, eliminando possíveis erros 
+              durante a inserção manual e assegurando a conformidade com as normas estabelecidas pelo RADOC.
+              </h3>
             </div>
-            <div>
-              <a href="#">
-                <button>Continuar</button>
-              </a>
+            <div className={classes.subTextos2}>
+              <h2>
+                Extração de Dados de Documentos
+              </h2>
+              <h3>
+                Efetua a leitura e extração de dados de documentos comprobatórios, como certificados e relatórios, agilizando o 
+                processo de preenchimento do RADOC, aumentando a eficiência ao automatizar a entrada de informações, reduzindo o 
+                tempo necessário para completar o relatório.
+              </h3>
             </div>
           </div>
 
-          <div
-            className={classes.tabelaRadoc}
-            id={classes.tabelasDisciplinasMinistradas}
-          ></div>
+          <div className={classes.colunaIndividual}>
+            <div className={classes.subTextos1}>
+              <h2>
+                Automação de Tarefas
+              </h2>
+              <h3>
+                O sistema foi projetado para simplificar e agilizar o preenchimento de seções do RADOC, o que 
+                resulta em uma redução significativa no tempo e esforço exigidos para concluir a documentação de forma precisa e eficiente.
+              </h3>
+            </div>
+            <div className={classes.subTextos2}>
+              <h2>
+                Registro e Edição de Atividades
+              </h2>
+              <h3>
+                Permite ao docente registrar suas atividades de forma organizada e detalhada, facilitando a edição e 
+                atualização das atividades ao longo do período de relatado.
+              </h3>
+            </div>
+          </div>
         </div>
+
+        <div className={classes.buttons}>
+          <button>Crie seu Radoc</button>
+        </div>
+
       </div>
       <ToastContainer position="bottom-left" />
     </div>
