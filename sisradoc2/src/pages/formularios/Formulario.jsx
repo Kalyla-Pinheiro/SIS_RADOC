7e0818610c26b59d88d38a20fe8fb170c079f3c1
Joao Henrique <jh07102003@gmail.com>
2024-04-06 15:16:52 -0300

    tamanho sisradoc da topbar

--- sisradoc2/src/pages/formularios/Formulario.jsx
+++ sisradoc2/src/pages/formularios/Formulario.jsx
@@ -9,7 +9,24 @@ const Formulario = () => {
 
       <div className={classes.formularioContainer}>
         <div className={classes.titulo}>
-          <h1>Bem vindo, preencha seus formulários!</h1>
+          <h1>Formulários das Atividades!</h1>
+          <h3 className={classes.paragrafos}>
+            Na presente página, disponibilizam-se os formulários das atividades
+            passíveis de preenchimento. Para preencher uma seção específica,
+            basta clicar no botão correspondente a cada atividade. Além disso, é
+            possível visualizar a distribuição da carga horária semanal das
+            atividades do ano em preenchimento ao clicar no botão "Visualizar
+            Distribuição da CH Semanal".
+          </h3>
+          <h3 className={classes.paragrafos}>
+            Ao concluir o preenchimento, clique em "Gerar Radoc" para finalizar
+            o preenchimento do ano em questão.
+          </h3>
+        </div>
+        <div>
+          <a href="/formulario">
+            <button>Gerar Radoc</button>
+          </a>
         </div>
       </div>
     </div>
