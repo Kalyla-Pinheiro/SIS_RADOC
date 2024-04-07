03f05e067197cda7b282406a60c35315d2556292
Joao Henrique <jh07102003@gmail.com>
2024-04-06 22:22:13 -0300

    tela de home atualizada

--- sisradoc2/src/components/Modal/home/ModalCrieSeuRadoc.jsx
+++ sisradoc2/src/components/Modal/home/ModalCrieSeuRadoc.jsx
@@ -1,12 +1,27 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, FormControl, FormLabel, Input, Box } from "@chakra-ui/react";
-import { useState, useContext} from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Box,
+} from "@chakra-ui/react";
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AnoContext } from "../../../utils/AnoContext";
 
 const ModalCrieSeuRadoc = ({ isOpen, onClose }) => {
   const navigate = useNavigate();
 
-  const [inputAno, setInputAno] = useState('');
+  const [inputAno, setInputAno] = useState("");
+  const [pergunta, setPergunta] = useState("");
   const { ano, setAnoValue } = useContext(AnoContext);
 
   const handleComecarClick = () => {
@@ -19,30 +34,49 @@ const ModalCrieSeuRadoc = ({ isOpen, onClose }) => {
 
   return (
     <>
-        <Modal isOpen={isOpen} onClose={onClose} isCentered>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>ATENÇÃO</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <FormControl display="flex" flexDir="column" gap={4}>
-                <Box>
-                  <FormLabel>Qual ano do Radoc você deseja preencher?</FormLabel>
-                  <Input type="text" placeholder="Ano" value={inputAno} onChange={(e) => setInputAno(e.target.value)}/>
-                </Box>
-              </FormControl>
-            </ModalBody>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>ATENÇÃO</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl display="flex" flexDir="column" gap={4}>
+              <Box>
+                <FormLabel>Qual ano do Radoc você deseja preencher?</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="Ano"
+                  value={inputAno}
+                  onChange={(e) => setInputAno(e.target.value)}
+                />
+              </Box>
+              <Box>
+                <FormLabel>
+                  Teve progressão ou promoção no último semestre deste RADOC?
+                </FormLabel>
+                <Select
+                  name="Funcao"
+                  placeholder="Pergunta"
+                  value={pergunta}
+                  onChange={(e) => setPergunta(e.target.value)}
+                >
+                  <option>Sim</option>
+                  <option>Não</option>
+                </Select>
+              </Box>
+            </FormControl>
+          </ModalBody>
 
-            <ModalFooter justifyContent="start">
-              <Button colorScheme="green" mr={3} onClick={handleComecarClick}>
-                COMEÇAR
-              </Button>
-              <Button colorScheme="red" onClick={onClose}>
-                CANCELAR
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+          <ModalFooter justifyContent="start">
+            <Button colorScheme="green" mr={3} onClick={handleComecarClick}>
+              COMEÇAR
+            </Button>
+            <Button colorScheme="red" onClick={onClose}>
+              CANCELAR
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
