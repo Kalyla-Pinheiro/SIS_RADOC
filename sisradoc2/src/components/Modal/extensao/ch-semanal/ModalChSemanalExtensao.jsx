606c679e5c68764826dcd815e4c53ee4921902e0
Joao Henrique <jh07102003@gmail.com>
2024-04-17 20:39:14 -0300

    botões

--- sisradoc2/src/components/Modal/extensao/ch-semanal/ModalChSemanalExtensao.jsx
+++ sisradoc2/src/components/Modal/extensao/ch-semanal/ModalChSemanalExtensao.jsx
@@ -1,109 +1,108 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Box,
-  } from "@chakra-ui/react";
-  import React, { useState, useContext, useEffect } from "react";
-  import { AnoContext } from "../../../../utils/AnoContext";
-  
-  const ModalChSemanalExtensao = ({
-    isOpen,
-    onClose,
-  }) => {
-    //const [ano, setAno] = useState(dataEdit.ano || "");
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+import React, { useState, useContext, useEffect } from "react";
+import { AnoContext } from "../../../../utils/AnoContext";
 
-    const { ano } = useContext(AnoContext);
+const ModalChSemanalExtensao = ({ isOpen, onClose }) => {
+  //const [ano, setAno] = useState(dataEdit.ano || "");
 
-    const localStorageKey = `${ano}`;
-    let localStorageData = localStorage.getItem(localStorageKey);
-    localStorageData = localStorageData ? JSON.parse(localStorageData) : {};
+  const { ano } = useContext(AnoContext);
 
-    const Extensao_AtividadesNaoFormais = localStorageData.ExtensaoChAtividadesnaoFormais;
-    const Extensao_EstagioExtensao = localStorageData.ExtensaoChEstagioExtensao;
-    const Extensao_OutrasAtividades = localStorageData.ExtensaoChOutrasAtividades;
-  
-  
-    localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
+  const localStorageKey = `${ano}`;
+  let localStorageData = localStorage.getItem(localStorageKey);
+  localStorageData = localStorageData ? JSON.parse(localStorageData) : {};
 
-    let TodoSemestre1Extensao = 0;
-    let TodoSemestre2Extensao = 0;
+  const Extensao_AtividadesNaoFormais =
+    localStorageData.ExtensaoChAtividadesnaoFormais;
+  const Extensao_EstagioExtensao = localStorageData.ExtensaoChEstagioExtensao;
+  const Extensao_OutrasAtividades = localStorageData.ExtensaoChOutrasAtividades;
 
-    if (Extensao_AtividadesNaoFormais) {
-      TodoSemestre1Extensao += Extensao_AtividadesNaoFormais["1Semestre"];
-      TodoSemestre2Extensao += Extensao_AtividadesNaoFormais["2Semestre"];
-    }
+  localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
 
-    if (Extensao_EstagioExtensao) {
-      TodoSemestre1Extensao += Extensao_EstagioExtensao["ChTotal"]/2;
-      TodoSemestre2Extensao += Extensao_EstagioExtensao["ChTotal"]/2;
-    }
+  let TodoSemestre1Extensao = 0;
+  let TodoSemestre2Extensao = 0;
 
-    if (Extensao_OutrasAtividades) {
-      TodoSemestre1Extensao += Extensao_OutrasAtividades["1Semestre"];
-      TodoSemestre2Extensao += Extensao_OutrasAtividades["2Semestre"];
-    }
+  if (Extensao_AtividadesNaoFormais) {
+    TodoSemestre1Extensao += Extensao_AtividadesNaoFormais["1Semestre"];
+    TodoSemestre2Extensao += Extensao_AtividadesNaoFormais["2Semestre"];
+  }
 
+  if (Extensao_EstagioExtensao) {
+    TodoSemestre1Extensao += Extensao_EstagioExtensao["ChTotal"] / 2;
+    TodoSemestre2Extensao += Extensao_EstagioExtensao["ChTotal"] / 2;
+  }
 
+  if (Extensao_OutrasAtividades) {
+    TodoSemestre1Extensao += Extensao_OutrasAtividades["1Semestre"];
+    TodoSemestre2Extensao += Extensao_OutrasAtividades["2Semestre"];
+  }
 
-
-    const gerarJSON = () => {
-      const updatedLocalStorageData = {
-        ...localStorageData,
-        EXTENSAO_CH_SEMANAL: {
-          "1Semestre": TodoSemestre1Extensao,
-          "2Semestre": TodoSemestre2Extensao,
-        }
-      };
-    
-      localStorage.setItem(localStorageKey, JSON.stringify(updatedLocalStorageData));
+  const gerarJSON = () => {
+    const updatedLocalStorageData = {
+      ...localStorageData,
+      EXTENSAO_CH_SEMANAL: {
+        "1Semestre": TodoSemestre1Extensao,
+        "2Semestre": TodoSemestre2Extensao,
+      },
     };
-  
-    return (
-      <>
-        <Modal isOpen={isOpen} onClose={onClose} isCentered >
-        
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader textAlign="center" marginTop={10}>CH SEMANAL DE EXTENSÃO</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <FormControl display="flex" gap={10} marginBottom={10}>
-                <Box>
-                  <FormLabel textAlign="center">1º Semestre</FormLabel>
-                  <Input
-                    type="text"
-                    placeholder="CH"
-                    value={TodoSemestre1Extensao}
-                  />
-                </Box>
-                <Box>
-                  <FormLabel textAlign="center">2º Semestre</FormLabel>
-                  <Input
-                    type="text"
-                    placeholder="CH"
-                    value={TodoSemestre2Extensao}
-                  />
-                </Box>
-              </FormControl>
-            </ModalBody>
-            <ModalFooter justifyContent="center">
-            {" "}
-            <Button colorScheme="green" onClick={gerarJSON()}>Salvar</Button>{" "}
-          </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
+
+    localStorage.setItem(
+      localStorageKey,
+      JSON.stringify(updatedLocalStorageData)
     );
   };
-  
-  export default ModalChSemanalExtensao;
-  
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign="center" marginTop={10}>
+            CH SEMANAL DE EXTENSÃO
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl display="flex" gap={10} marginBottom={3}>
+              <Box>
+                <FormLabel textAlign="center">1º Semestre</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="CH"
+                  value={TodoSemestre1Extensao}
+                />
+              </Box>
+              <Box>
+                <FormLabel textAlign="center">2º Semestre</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="CH"
+                  value={TodoSemestre2Extensao}
+                />
+              </Box>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter justifyContent="center">
+            {" "}
+            <Button colorScheme="green" onClick={gerarJSON()}>
+              Salvar
+            </Button>{" "}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalChSemanalExtensao;
--- sisradoc2/src/components/Modal/gestao/ch-semanal/ModalChSemanalGestao.jsx
+++ sisradoc2/src/components/Modal/gestao/ch-semanal/ModalChSemanalGestao.jsx
@@ -1,99 +1,95 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Box,
-  } from "@chakra-ui/react";
-  import React, { useState, useContext, useEffect } from "react";
-  import { AnoContext } from "../../../../utils/AnoContext";
-  
-  const ModalChSemanalGestao = ({
-    isOpen,
-    onClose,
-  }) => {
-    //const [ano, setAno] = useState(dataEdit.ano || "");
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+import React, { useState, useContext, useEffect } from "react";
+import { AnoContext } from "../../../../utils/AnoContext";
 
+const ModalChSemanalGestao = ({ isOpen, onClose }) => {
+  //const [ano, setAno] = useState(dataEdit.ano || "");
 
+  const { ano } = useContext(AnoContext);
 
-    const { ano } = useContext(AnoContext);
+  const localStorageKey = `${ano}`;
+  let localStorageData = localStorage.getItem(localStorageKey);
+  localStorageData = localStorageData ? JSON.parse(localStorageData) : {};
 
-    const localStorageKey = `${ano}`;
-    let localStorageData = localStorage.getItem(localStorageKey);
-    localStorageData = localStorageData ? JSON.parse(localStorageData) : {};
+  const Ch_Gestao_Representacao = localStorageData.GestãoChTotal;
 
-    const Ch_Gestao_Representacao = localStorageData.GestãoChTotal;
-  
-  
-    localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
+  localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
 
-    let TodoSemestre1Gestao = 0;
-    let TodoSemestre2Gestao = 0;
+  let TodoSemestre1Gestao = 0;
+  let TodoSemestre2Gestao = 0;
 
-    if (Ch_Gestao_Representacao) {
-      TodoSemestre1Gestao += Ch_Gestao_Representacao["1Semestre"];
-      TodoSemestre2Gestao += Ch_Gestao_Representacao["2Semestre"];
-    }
+  if (Ch_Gestao_Representacao) {
+    TodoSemestre1Gestao += Ch_Gestao_Representacao["1Semestre"];
+    TodoSemestre2Gestao += Ch_Gestao_Representacao["2Semestre"];
+  }
 
-
-
-    const gerarJSON = () => {
-      const updatedLocalStorageData = {
-        ...localStorageData,
-        GESTAO_CH_SEMANAL: {
-          "1Semestre": TodoSemestre1Gestao,
-          "2Semestre": TodoSemestre2Gestao,
-        }
-      };
-    
-      localStorage.setItem(localStorageKey, JSON.stringify(updatedLocalStorageData));
+  const gerarJSON = () => {
+    const updatedLocalStorageData = {
+      ...localStorageData,
+      GESTAO_CH_SEMANAL: {
+        "1Semestre": TodoSemestre1Gestao,
+        "2Semestre": TodoSemestre2Gestao,
+      },
     };
 
-  
-    return (
-      <>
-        <Modal isOpen={isOpen} onClose={onClose} isCentered >
-        
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader textAlign="center" marginTop={10}>CH SEMANAL DE GESTÃO</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <FormControl display="flex" gap={10} marginBottom={10}>
-                <Box>
-                  <FormLabel textAlign="center">1º Semestre</FormLabel>
-                  <Input
-                    type="text"
-                    placeholder="CH"
-                    value={TodoSemestre1Gestao}
-                  />
-                </Box>
-                <Box>
-                  <FormLabel textAlign="center">2º Semestre</FormLabel>
-                  <Input
-                    type="text"
-                    placeholder="CH"
-                    value={TodoSemestre2Gestao}
-                  />
-                </Box>
-              </FormControl>
-            </ModalBody>
-            <ModalFooter justifyContent="center">
-              {" "}
-              <Button colorScheme="green" onClick={gerarJSON()}>Salvar</Button>{" "}
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
+    localStorage.setItem(
+      localStorageKey,
+      JSON.stringify(updatedLocalStorageData)
     );
   };
-  
-  export default ModalChSemanalGestao;
-  
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign="center" marginTop={10}>
+            CH SEMANAL DE GESTÃO
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl display="flex" gap={10} marginBottom={3}>
+              <Box>
+                <FormLabel textAlign="center">1º Semestre</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="CH"
+                  value={TodoSemestre1Gestao}
+                />
+              </Box>
+              <Box>
+                <FormLabel textAlign="center">2º Semestre</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="CH"
+                  value={TodoSemestre2Gestao}
+                />
+              </Box>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter justifyContent="center">
+            {" "}
+            <Button colorScheme="green" onClick={gerarJSON()}>
+              Salvar
+            </Button>{" "}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalChSemanalGestao;
