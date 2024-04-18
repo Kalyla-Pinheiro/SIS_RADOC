443cddbc5ba23fc4744d4f953213b59c1eb44408
Polar0471 <rodrigoarth512@gmail.com>
2024-04-18 17:22:02 -0300

    Inserindo mensagens de erro nas últimas telas de formulários

--- sisradoc2/src/pages/outros/qualificacao-docente/QualificacaoDocente.jsx
+++ sisradoc2/src/pages/outros/qualificacao-docente/QualificacaoDocente.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../../../css-modules/Outros.module.css";
 import Navegacao from "../../../components/Navegação/Navegacao";
 import { BsQuestionCircleFill } from "react-icons/bs";
-import { ChakraProvider, Box } from "@chakra-ui/react";
+import { ChakraProvider, Box, useDisclosure } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 import TabelasQualificacaoDocente from "../../../formularios/outros/qualificacao-docente/TabelasQualificacaoDocente";
+import { ToastifyMessages } from "../../../utils/ToastifyMessages";
+import { ToastContainer } from "react-toastify";
 
 const QualificacaoDocente = () => {
+
+  const [pdfQualificacaoDocente, setPdfQualificacaoDocente] = useState(null);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handlepdfQualificacaoDocenteChange = (event) => {
+    setPdfQualificacaoDocente(event.target.files[0]);
+  };
+
+  const handleQualificacaoDocente = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("file", pdfQualificacaoDocente);
+    
+    if (pdfQualificacaoDocente === null) {
+      ToastifyMessages.warning(
+        "Campo vazio, por favor selecione um PDF para submeter!"
+      );
+    }
+  }
+
   const theme = extendTheme({
     styles: {
       global: {
@@ -37,14 +60,15 @@ const QualificacaoDocente = () => {
           action=""
           method="post"
           encType="multipart/form-data"
+          onSubmit={handleQualificacaoDocente}
         >
           <div className={classes.anexarPdfs}>
             <div className={classes.inputsPdfs}>
-              <input type="file" accept=".pdf" />
+              <input type="file" accept=".pdf" onChange={handlepdfQualificacaoDocenteChange}/>
               <p>Campo de submissão (PDF)</p>
             </div>
             <div className={classes.buttonSubmeterPDF}>
-              <button type="submit">Submeter PDF</button>
+              <button type="submit" onClick={handleQualificacaoDocente}>Submeter PDF</button>
             </div>
           </div>
         </form>
@@ -68,6 +92,7 @@ const QualificacaoDocente = () => {
           </div>
         </div>
       </div>
+      <ToastContainer position="bottom-left" />
     </div>
   );
 };
